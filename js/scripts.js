ccdf90a648da58a1f0775d1e2e35f52d211f90c3
jessie bell waite and james osborn <jessie@>
2017-09-25 15:58:01 -0700

    Add js to remove parent of this element

--- js/scripts.js
+++ js/scripts.js
@@ -6,22 +6,30 @@ function ToDoList (listItem, listNotes) {
 $(document).ready(function(){
   $("form#list-form").submit(function(event) {
     event.preventDefault();
-
+    console.log($(this));
     var inputListItem = $("input#new-list-item").val();
     var inputListNotes = $("input#new-note").val();
 
     var newItem = new ToDoList(inputListItem, inputListNotes);
 
-    $("ul#items").append("<li><span class='item'>" + newItem.listItem + "</spam></li>");
+    $("ul#items").append(
+      "<li><span class='item'>" +
+      newItem.listItem +
+      "</span>" +
+       "<span class='remover'> Complete" +
+      "</span></li>");
 
     $("input#new-list-item").val("");
     $("input#new-note").val("");
     $(".item").last().click(function(){
-
       $("#show-items").show();
-      $("show-items h2").text(newItem.listItem);
+      $("#show-items h2").text(newItem.listItem);
       $(".items").text(newItem.listItem);
       $(".notes").text(newItem.listNotes);
+    })
+
+    $(".remover").last().click(function(){
+      $(this).parent().remove();
     })
   })
 })
